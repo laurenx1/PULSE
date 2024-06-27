875b0a1aba885bebdfb11fc68aa90229d08fdd51
Lauren Nia Pryor <lnpryor@meta.com>
2024-06-27 14:03:03 -0700

    backend for hashing and matching passwords using bcrypt complete

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,26 @@
+import { hashPassword, checkPassword } from './middleware';
+
 const express = require('express');
+const bcrypt = require('bcrypt');
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(express.json());
+
+
+app.use(express);
+app.use(hashPassword);
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+app.post('/users', hashPassword, async (req, res) => {
+    const user = await prisma.user.create({ data: req.body });
+    res.json(user);
+  });
+
+
+// POST request to server with login credentials in request body
+app.post('/login', checkPassword);
